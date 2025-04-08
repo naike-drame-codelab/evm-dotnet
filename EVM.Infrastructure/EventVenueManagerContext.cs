9dac4bb6722ad77b459a0629351a5ade4d6aa08a
Naïké Dramé <naike.drame.dev@gmail.com>
2025-04-08 11:22:36 +0200

    Add project files.

---
+++ EVM.Infrastructure/EventVenueManagerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EVM.Infrastructure
+{
+    public class EventVenueManagerContext(DbContextOptions options) : DbContext(options)
+    {
+        // DbSets for the entities
+        // OnModelCreating method to configure the model
+    }
+}
